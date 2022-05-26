3cec2eed09fa45da49646dda69fadef243beff6a
markthree <1801982702@qq.com>
2022-05-26 17:11:16 +0800

    feat: add untypes

--- src/type.ts
+++ src/type.ts
@@ -5,3 +5,29 @@ export type AsyncFunciton<T extends AnyFunction> = (
 ) => Promise<ReturnType<T>>
 
 export type AnyAsyncFunction = AsyncFunciton<AnyFunction>
+
+export type UnArray<T> = T extends Array<infer P> ? P : T
+
+export type UnSet<T> = T extends Set<infer P> ? P : T
+
+export type UnWeakSet<T> = T extends WeakSet<infer P>
+	? P
+	: T
+
+export type UnMap<T> = T extends Map<infer K, infer V>
+	? [K, V]
+	: T
+
+export type UnWeakMap<T> = T extends WeakMap<
+	infer K,
+	infer V
+>
+	? [K, V]
+	: T
+
+export type UnPromise<T> = T extends Promise<infer P>
+	? P
+	: T
+
+export type UnPromiseReturnType<T extends AnyFunction> =
+	UnPromise<ReturnType<T>>
