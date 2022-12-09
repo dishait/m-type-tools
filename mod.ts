bd4437b9df0346f6526ad61dfe1b488f4e508f90
markthree <1801982702@qq.com>
2022-12-09 14:39:38 +0800

    chore: change file structure

---
+++ mod.ts
@@ -0,0 +1,7 @@
+// 支持 deno
+
+export * from './src/is'
+export * from './src/un'
+export * from './src/pick'
+export * from './src/array'
+export * from './src/function'
--- src/mod.ts
+++
@@ -1,7 +0,0 @@
-// 支持 deno
-
-export * from './is'
-export * from './un'
-export * from './pick'
-export * from './array'
-export * from './function'
