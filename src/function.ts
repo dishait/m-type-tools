a2a3fa573f69b85f79f58ed09d9a0840f3b02442
markthree <1801982702@qq.com>
2023-04-02 18:30:34 +0800

    feat: add isPromise

--- src/function.ts
+++ src/function.ts
@@ -1,7 +1,7 @@
-export type AnyFunction = (...args: any) => any
+export type AnyFunction = (...args: any) => any;
 
 export type AsyncFunciton<T extends AnyFunction> = (
-	...args: Parameters<T>
-) => Promise<ReturnType<T>>
+  ...args: Parameters<T>
+) => Awaited<ReturnType<T>>;
 
-export type AnyAsyncFunction = AsyncFunciton<AnyFunction>
+export type AnyAsyncFunction = AsyncFunciton<AnyFunction>;
