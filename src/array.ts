933875ce83365d4fd897da52f76f34df63f6b25e
markthree <1801982702@qq.com>
2023-12-30 18:30:44 +0800

    feat(array): add Arrayable

--- src/array.ts
+++ src/array.ts
@@ -1,34 +1,30 @@
+export type Arrayable<T> = T | Array<T>;
+
 export type ItemExist<V, A = []> = A extends [
-	infer F,
-	...infer P
-]
-	? V extends F
-		? V
-		: ItemExist<V, P>
-	: never
+  infer F,
+  ...infer P,
+] ? V extends F ? V
+  : ItemExist<V, P>
+  : never;
 
 export type ItemNotExist<V, A = []> = A extends [
-	infer F,
-	...infer P
-]
-	? V extends F
-		? never
-		: ItemNotExist<V, P>
-	: V
+  infer F,
+  ...infer P,
+] ? V extends F ? never
+  : ItemNotExist<V, P>
+  : V;
 
 export type FilterNeverInArray<
-	T,
-	G extends unknown[] = []
-> = T extends [infer F, ...infer P]
-	? FilterNeverInArray<
-			P,
-			[F] extends [never] ? [...G] : [...G, F]
-	  >
-	: G
+  T,
+  G extends unknown[] = [],
+> = T extends [infer F, ...infer P] ? FilterNeverInArray<
+    P,
+    [F] extends [never] ? [...G] : [...G, F]
+  >
+  : G;
 
 export type UniqueArray<
-	T,
-	G extends unknown[] = []
-> = T extends [...infer P, infer L]
-	? UniqueArray<P, [ItemNotExist<L, P>, ...G]>
-	: FilterNeverInArray<G>
+  T,
+  G extends unknown[] = [],
+> = T extends [...infer P, infer L] ? UniqueArray<P, [ItemNotExist<L, P>, ...G]>
+  : FilterNeverInArray<G>;
